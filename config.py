5258dcf099d4749bfe1b11be877e383f3274d885
MoeenH <57503695+MoeenH@users.noreply.github.com>
2023-05-12 15:44:55 +0500

    project report

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
-#CLIENT_ID = "your_client_id_here"
-#CLIENT_SECRET = "your_client_secret_here"
-#USER_AGENT = "your_user_agent_here"
+CLIENT_ID = "your_client_id_here"
+CLIENT_SECRET = "your_client_secret_here"
+USER_AGENT = "your_user_agent_here"
 
 
   
     #Moeen Credentials 
-CLIENT_ID="ml3JKWrmbdnHChAxNziJjg"
-CLIENT_SECRET="2cXTppbxDoSnvOBLpnqvmm1XnMAZng"
-USER_AGENT="my user agent"
+#CLIENT_ID="ml3JKWrmbdnHChAxNziJjg"
+#CLIENT_SECRET="2cXTppbxDoSnvOBLpnqvmm1XnMAZng"
+#USER_AGENT="my user agent"
    # )
 
     #Mustafa credentials.
