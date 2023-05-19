3c3f793715a236979c6e25e2a70047e11be97c57
MoeenH <57503695+MoeenH@users.noreply.github.com>
2023-05-20 01:19:01 +0500

    config

--- config.py
+++ config.py
@@ -4,14 +4,4 @@ USER_AGENT = "your_user_agent_here"
 
 
   
-    #Moeen Credentials 
-#CLIENT_ID="ml3JKWrmbdnHChAxNziJjg"
-#CLIENT_SECRET="2cXTppbxDoSnvOBLpnqvmm1XnMAZng"
-#USER_AGENT="my user agent"
-   # )
-
-    #Mustafa credentials.
- #       client_id = "W3vT8epgjWSF6DhkudzmjA", 
-  #      client_secret = "h8X2dVt37L6ZXHRKi2K_tYy1NAozHg", 
-   #     user_agent = "My User Agent."
-    
+   
